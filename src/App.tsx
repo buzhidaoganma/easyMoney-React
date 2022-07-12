f0935ca76245c67fe41047e6bd0bd469d59ddc26
郑李畴 <2678646597@qq.com>
2022-07-13 00:45:37 +0800

    搞定icon 的样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Nav from "components/Nav";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+require("icons/money.svg");
+require("icons/tag.svg");
+require("icons/chart.svg");
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -9,7 +12,15 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -19,12 +30,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签页面</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页面</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/statistics">统计页面</Link>
         </li>
       </ul>
