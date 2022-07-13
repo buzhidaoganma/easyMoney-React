dac44ad1e7cc8b24b04cfc0094b682b08094d73d
郑李畴 <2678646597@qq.com>
2022-07-13 18:57:38 +0800

    import 整个icons目录

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-require("icons/money.svg");
-require("icons/tag.svg");
-require("icons/chart.svg");
+import Icon from "./Icon";
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -30,21 +28,15 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#tag" />
-          </svg>
+          <Icon name="tag" />
           <Link to="/tags">标签页面</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#money" />
-          </svg>
+          <Icon name="money" />
           <Link to="/money">记账页面</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#chart" />
-          </svg>
+          <Icon name="chart" />
           <Link to="/statistics">统计页面</Link>
         </li>
       </ul>
