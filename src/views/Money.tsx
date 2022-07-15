f5a2a59d64fbc127e82a8010c9642ff64e482b72
郑李畴 <2678646597@qq.com>
2022-07-16 03:43:33 +0800

    Money.tsx中的NumberpadSection UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -68,7 +68,66 @@ const CategorySection = styled.section`
     }
   }
 `;
-const NumberPadSection = styled.section``;
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .output {
+    //这里面有两个div所以给它们类名字，用.来表示
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+      inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+    //另外两个-5px是为了只让下面有阴影,下面的代码是为了上边也有内阴影
+  }
+  > .pad {
+    //这里不能用flex定位，因为一个ok键位需要占的高度为两个普通按钮的高度，用flex定位会导致空出一行
+    > button {
+      border: none;
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      &.ok {
+        float: right;
+        height: 128px;
+      }
+      &.zero {
+        width: 50%;
+      }
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: #e0e0e0;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: #d3d3d3;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: #c1c1c1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: #b8b8b8;
+      }
+      &:nth-child(12) {
+        background: #9a9a9a;
+      }
+      &:nth-child(14) {
+        background: #a9a9a9;
+      }
+    }
+  }
+`;
 
 function Money() {
   return (
@@ -95,8 +154,8 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className="output">100</div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -108,8 +167,8 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
