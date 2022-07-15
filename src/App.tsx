3beefe4ec1a48de28aa395f7f373870024e72724
郑李畴 <2678646597@qq.com>
2022-07-15 16:30:10 +0800

    Money.tsx中的TagSection UI 完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 import Money from "views/Money";
 import NoMatch from "views/NoMatch";
 import Statistics from "views/Statistics";
 import Tags from "views/Tags";
 
+const AppWrapper = styled.div`
+  color: #333;
+`;
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/money" element={<Money />}></Route>
-        <Route path="/tags" element={<Tags />}></Route>
-        <Route path="/statistics" element={<Statistics />}></Route>
-        <Route path="/" element={<Money />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Routes>
+          <Route path="/money" element={<Money />}></Route>
+          <Route path="/tags" element={<Tags />}></Route>
+          <Route path="/statistics" element={<Statistics />}></Route>
+          <Route path="/" element={<Money />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
+    </AppWrapper>
   );
 }
 
