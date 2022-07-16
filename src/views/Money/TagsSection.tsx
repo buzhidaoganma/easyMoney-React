4e7629623de44cc7593f0a1e0b3f2980f9342904
郑李畴 <2678646597@qq.com>
2022-07-17 02:15:56 +0800

    完成标签选择和新增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #ffffff;
   padding: 12px 16px;
   flex-grow: 1; //这句话和底下的Layout呼应，有多余的空间就给标签
@@ -17,6 +18,9 @@ const TagsSection = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+      &.selected {
+        background: #f60;
+      }
     }
   }
   > button {
@@ -28,5 +32,42 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
+
+const TagsSection: React.FC = (props) => {
+  const [tags, setTags] = useState<string[]>(["衣", "食", "住", "行"]);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]); //设置选中功能
+  const onAddTag = () => {
+    const tagName = window.prompt("新标签的名称为");
+    if (tagName !== null) {
+      setTags([...tags, tagName]);
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      setSelectedTags(selectedTags.filter((t) => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map((tag) => (
+          <li
+            key={tag}
+            onClick={() => {
+              onToggleTag(tag);
+            }}
+            className={selectedTags.indexOf(tag) >= 0 ? "selected" : ""}
+          >
+            {tag}
+          </li>
+        ))}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
 
 export default TagsSection;
