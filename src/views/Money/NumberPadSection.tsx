f4539bdaec13a58f1da494a75ec634d32061c28f
郑李畴 <2678646597@qq.com>
2022-07-18 01:03:42 +0800

    重构NuberPadSection.tsx

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,66 +1,7 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import generateOutput from "./NumberPadSection/generateOutput";
+import Wrapper from "./NumberPadSection/NumberPadWrapper";
 
-const Wrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  > .output {
-    //这里面有两个div所以给它们类名字，用.来表示
-    background: white;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
-      inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-    //另外两个-5px是为了只让下面有阴影,下面的代码是为了上边也有内阴影
-  }
-  > .pad {
-    //这里不能用flex定位，因为一个ok键位需要占的高度为两个普通按钮的高度，用flex定位会导致空出一行
-    > button {
-      border: none;
-      font-size: 18px;
-      float: left;
-      width: 25%;
-      height: 64px;
-      &.ok {
-        float: right;
-        height: 128px;
-      }
-      &.zero {
-        width: 50%;
-      }
-      &:nth-child(1) {
-        background: #f2f2f2;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #e0e0e0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: #d3d3d3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: #c1c1c1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: #b8b8b8;
-      }
-      &:nth-child(12) {
-        background: #9a9a9a;
-      }
-      &:nth-child(14) {
-        background: #a9a9a9;
-      }
-    }
-  }
-`;
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = useState<string>("0");
   const setOutput = (output: string) => {
@@ -76,44 +17,13 @@ const NumberPadSection: React.FC = () => {
     if (text === null) {
       return;
     }
-    switch (text) {
-      case "0":
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-        if (output === "0") {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case ".":
-        if (output.indexOf(".") >= 0) {
-          return;
-        }
-        setOutput(output + text);
-        break;
-      case "删除":
-        if (output.length === 1) {
-          setOutput("");
-        } else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case "清空":
-        setOutput("");
-        break;
-      case "OK":
-        break;
+    if (text === "OK") {
+      //TODO
+      return;
     }
-  };
 
+    setOutput(generateOutput(text, output));
+  };
   return (
     <Wrapper>
       <div className="output">{output}</div>
