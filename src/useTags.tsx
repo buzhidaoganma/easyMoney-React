edbcf0ce499ad8eb65e64caa5d65b25da2738f15
郑李畴 <2678646597@qq.com>
2022-07-19 02:49:25 +0800

    完成tag List样式

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 const useTags = () => {
   const [tags, setTags] = useState<string[]>(["衣", "食", "住", "行"]);
   return {
-    tags: tags,
-    setTags: setTags,
+    tags,
+    setTags,
   };
 };
 
