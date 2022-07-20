145fb57b820613d01655d9cae4f1e9810c5904ff
郑李畴 <2678646597@qq.com>
2022-07-21 02:33:04 +0800

    编辑标签更改标签名

--- src/Tag.tsx
+++ src/Tag.tsx
@@ -38,7 +38,12 @@ const Tag: React.FC = () => {
         {/* 这个空的 */}
       </Topbar>
       <InputWrapper>
-        <Input label="标签名" type="text" placeholder="标签名" />
+        <Input
+          label="标签名"
+          type="text"
+          placeholder="标签名"
+          value={tag.name}
+        />
       </InputWrapper>
       <Center>
         <Space />
